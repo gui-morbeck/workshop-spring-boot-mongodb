9a695575325a339f2c9d3ac5561eb4e7abce98d2
Guilherme Morbeck <morbeck719@gmail.com>
2022-08-30 14:42:22 -0300

    Project created

---
+++ src/test/java/com/centrooleo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centrooleo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
