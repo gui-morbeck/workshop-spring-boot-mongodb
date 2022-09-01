03448688a327a6d7b842191f45fde5c47148ffb1
Guilherme Morbeck <morbeck719@gmail.com>
2022-09-01 12:58:59 -0300

    Obtendo um post por id

---
+++ src/main/java/com/centrooleo/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.centrooleo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.centrooleo.workshopmongo.domain.Post;
+import com.centrooleo.workshopmongo.repositories.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> opt = repository.findById(id);
+		return opt.get();
+	}
+	
+}
